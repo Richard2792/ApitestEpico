fb15072a33855efeafa05a5d785f54f22d871692
Richard2792 <83260577+Richard2792@users.noreply.github.com>
2025-02-09 14:01:19 -0600

    packaje.js

---
+++ packaje.js
@@ -0,0 +1,19 @@
+{
+  "name": "discord-proxy",
+  "version": "1.0.0",
+  "description": "API para reenviar webhooks de Discord desde Roblox Studio",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "prestart": "npm install",
+    "test": "echo \"No hay pruebas definidas aún\" && exit 1"
+  },
+  "dependencies": {
+    "express": "^4.21.2",
+    "axios": "^1.7.9"
+  },
+  "engines": {
+    "node": "18.x"
+  },
+  "license": "MIT"
+}
